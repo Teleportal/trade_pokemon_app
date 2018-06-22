00bd1058ab81d561a6cde00f52f2a6479e18e241
codeauslander <cesar.gusto.catano@gmail.com>
2018-06-21 20:16:26 -0500

    populating app with dummy data in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create!({
+#   friend_code:'123456789012',
+#   user_name:'Josh',
+#   avatar:'El Josh',
+#   contact_info:'home',
+#   location:'geocode',
+# })
+
+# User.create!({
+#   friend_code:'098765432109',
+#   user_name:'Joly',
+#   avatar:'El Joly',
+#   contact_info:'home',
+#   location:'geocode',
+# })
+
+# Pokemon.create!({
+#   species:'Bulbasaur'
+# })
+
+# Pokemon.create!({
+#   species:'Ivysaur'
+# })
+
+# OwnedPokemon.create!({
+#   user_id:1,
+#   nickname:'Sparky',
+#   combat_power:3000,
+#   pokemon_id:1
+# })
+
+# OwnedPokemon.create!({
+#   user_id:2,
+#   nickname:'anything',
+#   combat_power:5000,
+#   pokemon_id:2
+# })
+
+# DesiredPokemon.create!({
+#   user_id:1,
+#   min_cp:1000,
+#   pokemon_id:2
+# })
+
+# Offer.create!({
+#   owned_pokemon_id_1:1,
+#   owned_pokemon_id_2:2,
+#   status:1
+# })
