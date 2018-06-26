eb93908ab344bf92a1f0697b148073f9829ee245
Teleportal <joshthegreenberg@gmail.com>
2018-06-25 20:33:58 -0500

    quality of life model methods for species name

--- app/models/owned_pokemon.rb
+++ app/models/owned_pokemon.rb
@@ -3,4 +3,9 @@ class OwnedPokemon < ApplicationRecord
   belongs_to :pokemon
   has_many :offered_pokemons
   has_many :offers, :through => :offered_pokemons
+
+
+  def species
+    pokemon.species
+  end
 end
