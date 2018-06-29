d7315406faa3f7cf3fe542baaa3e9e359fa04c2e
codeauslander <cesar.gusto.catano@gmail.com>
2018-06-28 19:55:41 -0500

    Added Search Pokemons

--- client/src/components/Trade.js
+++ client/src/components/Trade.js
@@ -8,21 +8,68 @@ class Trade extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemons: []
+      pokemons: [],
+      all_pokemons: [],
+      desired_pokemons: [],
+      owned_pokemons: []
     };
   }
 
   componentDidMount() {
       axios.get("/pokemons.json")
       .then(response => {
-          console.log(response.data)
+          console.log('pokemons',response.data)
           this.setState({
-              pokemons: response.data
+              pokemons: response.data,
+              all_pokemons: response.data
+          })
+          console.log(this.state);
+      })
+      .catch(error => console.log(error,"error"))
+
+
+      axios.get("/desired_pokemons.json")
+      .then(response => {
+          console.log('desired_pokemons',response.data)
+          this.setState({
+              desired_pokemons: response.data
+          })
+          console.log(this.state);
+      })
+      .catch(error => console.log(error,"error"))
+
+      axios.get("/owned_pokemons.json")
+      .then(response => {
+          console.log('owned_pokemons',response.data)
+          this.setState({
+              owned_pokemons: response.data
           })
           console.log(this.state);
       })
       .catch(error => console.log(error,"error"))
   }
+
+  filterList(filterText) {
+    console.log(filterText.trim() == '');
+    if ( filterText.trim() == '' ) {
+      return this.state.all_pokemons;
+    } 
+    else {
+      return this.state.pokemons.filter(function(pokemon){
+        return pokemon.species.toLowerCase().search(
+          filterText.toLowerCase()) !== -1;
+      });
+    }
+  };
+
+  handleChange(event) {
+    const filteredPokemons = this.filterList(event.target.value);
+    this.setState({ pokemons: filteredPokemons });
+  }
+
+  
+  
+
 
   render() {
     const pokemons = this.state.pokemons;
@@ -31,7 +78,8 @@ class Trade extends Component {
         <TextField style={{padding: 24}}
                             id="searchInput"
                             placeholder="Search trade"   
-                            margin="normal">
+                            margin="normal"
+                            onChange={this.handleChange.bind(this)}>
         </TextField>
           Desired
         <Checkbox>
